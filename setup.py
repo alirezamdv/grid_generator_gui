922aba44c78a20fa6ad4edf7b6cc6c8e771e8a19
alirezamdv <ali.s3cret@gmail.com>
2021-10-26 10:03:30 +0200

    plugin

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         'netCDF4==1.5.7',
         'numpy==1.21.2',
         'pyside2==5.14.2.2',
+        'pyqt5-plugins==5.15.5.2.2',
         'PyQt5==5.15.5',
         'pyqtlet==0.3.3',
         'PyQtWebEngine==5.15.5'
